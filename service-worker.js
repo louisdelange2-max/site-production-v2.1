6f5afbb0fa8b3822d7a76796547dbc00ef4ab105
louisdelange2-max <louisdelange2@gmail.com>
2026-04-04 23:47:58 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "site-production-v2.1.9";
+const ASSETS = ["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+      return resp;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
